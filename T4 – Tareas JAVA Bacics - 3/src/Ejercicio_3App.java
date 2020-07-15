81eb2c029e5b8fee0526c0d0b4e26ba16889a20a
danielmac03-curso <danielmaciasgracia13@gmail.com>
2020-07-15 16:45:53 +0200

    Subido ejercicio 3 de T4 – Tareas JAVA Bacics

---
+++ T4 – Tareas JAVA Bacics - 3/src/Ejercicio_3App.java
@@ -0,0 +1,74 @@
+
+public class Ejercicio_3App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Escribe un programa Java que realice lo siguiente: declarar dos variables X e Y de tipo int,
+		dos variables N y M de tipo double y asigna a cada una un valor. A continuación muestra por
+		pantalla:*/
+		int x = 10, y = 3;
+		double n = 12.2, m = 15.7;
+		
+		
+		//El valor de cada variable.
+		System.out.println("El valor de X es igual a: " + x);
+		System.out.println("El valor de Y es igual a: " + y);
+		System.out.println("El valor de N es igual a: " + n);
+		System.out.println("El valor de M es igual a: " + m);
+		
+		//La suma X + Y
+		System.out.println("La suma de X y Y es: " + (x + y));
+		
+		//La diferencia X – Y
+		System.out.println("La diferencia de X y Y es: " + (x - y));
+		
+		//El producto X * Y
+		System.out.println("La producto de X y Y es: " + (x * y));
+		
+		//El cociente X / Y
+		System.out.println("La cociente de X y Y es: " + (x / y));
+		
+		//El resto X % Y
+		System.out.println("La resto de X y Y es: " + (x % y));
+		
+		//La suma N + M
+		System.out.println("La suma de N y M es: " + (n + m));
+		
+		//La diferencia N – M
+		System.out.println("La diferencia de N y M es: " + (n - m));
+		
+		//El producto N * M
+		System.out.println("La producto de N y M es: " + (n * m));
+		
+		//El cociente N / M
+		System.out.println("La cociente de N y M es: " + (n / m));
+		
+		//El resto N % M
+		System.out.println("La resto de N y M es: " + (n % m));
+		
+		//La suma X + N
+		System.out.println("La suma de X y N es: " + (x + n));
+		
+		//El cociente Y / M
+		System.out.println("La cociente de Y y M es: " + (y / m));
+		
+		//El resto X % M
+		System.out.println("La módulo de N y M es: " + (n % m));
+		
+		//El doble de cada variable
+		System.out.println("El doble de X es: " + (x*2));
+		System.out.println("El doble de Y es: " + (y*2));
+		System.out.println("El doble de N es: " + (n*2));
+		System.out.println("El doble de M es: " + (m*2));
+		
+		//La suma de todas las variables
+		System.out.println("La suma de todas las variables són: " + (x + y + n + m));
+		
+		//El producto de todas las variables
+		System.out.println("La producto de todas las variables són: " + (x * y * n * m));
+		
+		
+	}
+
+}
